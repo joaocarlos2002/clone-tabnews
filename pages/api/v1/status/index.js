d168c49ce81e84cca094a27cd7c3736d1b4df19a
João Carlos Garcia <145946837+joaocarlos2002@users.noreply.github.com>
2024-11-16 00:09:52 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js"
 
-async function index(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result)
-  response.status(200).json({key: "value"});
+async function status(request, response) {
+  const updatedAt = new Date().toISOString()
+
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+
+  const databaseMaxConnectionResult = await database.query("SHOW max_connections;")
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections
+
+  const databaseName = process.env.POSTGRES_DB
+  const dataabaseOpenConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const connectionsBeingUsedValueValue = dataabaseOpenConnectionsResults.rows[0].count
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionValue,
+        connections_being_used: connectionsBeingUsedValueValue,
+      },
+    },
+  });
 }
 
-export default index
+
+export default status
